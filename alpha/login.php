9a23a429459601507ca62d6b24016e0033f0da70
shaneavelino <shanecodev@gmail.com>
2020-02-14 12:05:37 -0500

    alpha features fully implemented

--- alpha/login.php
+++ alpha/login.php
@@ -1,30 +1,48 @@
 <?php
 
-  // read in a POST request
-  $incoming_post_request = file_get_contents('php://input');
-
-  // Response to send back to front end
-  $response = array("njitAuthenticated" => "false", "dbAuthenticated" => "false");
-
-  // decode the json object into a php array
-  $login_json_object = json_decode($incoming_post_request, true);
-
-  $njit_endpoint = 'https://myhub.njit.edu/vrs/ldapAuthenticateServlet';
-  $db_endpoint = 'https://web.njit.edu/~tg253/userservice.php';
+  /** initialize response body into an associative array */
+  $response = array('njitAuthenticated' => 'false', 'dbAuthenticated' => 'false');
   
-  // cURL the incoming POST request to the backend database
-  $db_response = curl_request($db_endpoint, $incoming_post_request);
+  /** decode the incoming login request into an associative array */
+  $db_post = file_get_contents('php://input');
+  $db_json = json_decode($db_post, true);
+
+  /** spoof njit html text body */
+  $njit_post = http_build_query(array('user_name' => $db_json['user'], 'passwd' => $db_json['password'], 'SUBMIT' => 'Login'));
+
+  /** cURL to db script */
+  $db_endpoint = 'https://web.njit.edu/~tg253/userservice.php';
+  $db_response = curl_post_request($db_endpoint, $db_post);
   $db_validation = json_decode($db_response, true);
-
+  
   // cURL the incoming POST request to an NJIT login server
-  $njit_response = curl_request($njit_endpoint, $incoming_post_request);
-  $njit_validation = json_decode($njit_response, true);
-  echo $njit_response;
+  $njit_endpoint = 'https://myhub.njit.edu/vrs/ldapAuthenticateServlet';
+  $njit_response = curl_post_request($njit_endpoint, $njit_post);
+  $njit_string = html_entity_decode($njit_response);
+  // no need to validate because njit response is in HTML for a bad login $njit_validation = json_decode($njit_response, true);
 
-  // params:
-  // $url: endpoint to curl
-  // $body: json object to post
-  function curl_request($url, $body) {
+    /** set up validation to send back to front end */
+  if ($db_validation['ValidUser'] == 'true') {
+    $response['dbAuthenticated'] = 'true';
+  } else {
+    $response['dbAuthenticated'] = 'false';
+  }
+
+  /** set up njit validation to send back to front end */
+  $missing_creds = 'Please';
+  $bad_creds = 'Invalid';
+  if (strpos($njit_string, $bad_creds) || strpos($njit_string, $missing_creds)) {
+    $response['njitAuthenticated'] = 'false';
+  } else {
+    $response['njitAuthenticated'] = 'true';
+  }
+
+  /** params:
+    * $url: endpoint to curl
+    * $body: post json object
+    * return: curl response
+    */
+  function curl_post_request($url, $body) {
     $ch = curl_init();
 
     curl_setopt_array($ch, array(
@@ -37,20 +55,7 @@
     return curl_exec($ch);
     curl_close($ch);
   }
-  
-  if ($db_validation['ValidUser'] == "true") {
-    $response["dbAuthenticated"] = "true";
-  } else {
-    $response["dbAuthenticated"] = "false";
-  }
 
-  // echo $db_response;
   echo json_encode($response);
-
-
-  // check if response authenticates the incoming request from the front end
-  // if db response of validuser is true set $response['njitAuthenticated'] to true
-  // else then set it to false
-  //echo json_encode($response);
 
 ?>
