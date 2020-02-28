c3cd3e72fc9304977dbd4d29e4d2b44bf49ba807
shanecodev <shanecodev@gmail.com>
2020-02-28 10:27:17 -0500

    add beta code

--- beta/grader.php
+++ beta/grader.php
@@ -10,8 +10,28 @@ class Grader {
 }
 
 $str = "def addTwoNumbers(i,j): return i + j";
+$test_case = "";
 
 $grader = Grader::gradeQuestion($str);
 echo $grader;
 
+
+
+
+/** params:
+  * $url: endpoint to curl
+  * $body: post json object
+  * return: curl response
+  */
+function curl_post_request($url, $body) {
+  $ch = curl_init();
+  curl_setopt_array($ch, array(
+    CURLOPT_URL => $url,
+    CURLOPT_RETURNTRANSFER => 1,
+    CURLOPT_POST => 1,
+    CURLOPT_POSTFIELDS => $body
+  ));
+  return curl_exec($ch);
+  curl_close($ch);
+}
 ?>
