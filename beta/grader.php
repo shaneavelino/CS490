d4c7b3f21883ecb7ef878fa8f78c2bc0a342987a
shaneavelino <shanecodev@gmail.com>
2020-03-24 19:48:22 -0400

    beta complete

--- beta/grader.php
+++ beta/grader.php
@@ -32,6 +32,7 @@ class Grader {
   private $input;
   private $output;
   private $final;
+  private $weight;
 
   function set_question($question) {
     $this->question = $question;
@@ -73,9 +74,20 @@ class Grader {
     return $this->final;
   }
 
+  function set_weight($weight) {
+    $this->weight = $weight;
+  }
+
+  function get_weight() {
+    return $this->weight;
+  }
+
   // return a grade for the student's raw answer based on correctness
-  public function grade_func_name($question, $answer) {
+  public function grade_func_name($question, $answer, $weight) {
     $score = 0;
+
+    // the function name is 25% of the score of each question
+    $function_weight = ($weight * 25)/100;
     
     // python def keyword (3 chars)
     $def_keyword = substr($answer, 0, 3);
@@ -88,7 +100,7 @@ class Grader {
 
     // ++score if the question is correct, else don't add score
     if ($question == substr($open_paren, strlen($def_keyword)+1)) {
-      $score += 10;
+      $score += $function_weight;
     } else {
       $score += 0;
     }
@@ -97,8 +109,11 @@ class Grader {
   }
 
   // return grade for the student's raw answer based on test cases
-  public function grade_test_case($input, $output, $answer, $question) {
+  public function grade_test_case($input, $output, $answer, $question, $weight) {
     $score = 0;
+
+    // each question has 2 test cases each so it's worth 75%/2
+    $test_case_weight = (($weight * 75)/100)/2;
 
     // need to format the question in the event the student misspelled the function name
     $def_keyword = substr($answer, 0, 3);
@@ -111,16 +126,16 @@ class Grader {
     $formatted_answer = "$before_colon\n\t$after_colon";
 
     // create python text file from input answer, append the function name with the test case input
-    $content = "$formatted_answer \n\n$student_question($input)";
+    $content = "$formatted_answer \n\nstring='$input'\nnumber=u'$input'\nif(string.isalpha()):\n\t$student_question(string)\nif(number.isnumeric()):\n\t$student_question(int(number))";
     $pythonfile = file_put_contents('exam.py', $content);
     
     // execute python text file
     // return result of file
     $student_result = shell_exec("python3 exam.py");
 
-    // if testcase correct, +5
-    if ($student_result = $output) {
-      $score += 5;
+    // if testcase is correct, +75% of the question - needed to trim the newline from the python output
+    if (trim($student_result) == $output) {
+      $score += $test_case_weight;
     } else {
       $score += 0;
     }
@@ -131,6 +146,14 @@ class Grader {
 
 /** This is where we will do the main logic of the auto grader */
 if (isset($json['exam']) && isset($json['user'])) {
+
+  // fetch each question's score from the exam
+  $score_endpoint = EXAM_URL . '?' . http_build_query(array('name' => $json['exam']));
+  $score_controller = new Controller();
+  $score_controller->setUrl($score_endpoint);
+  $score_curl = $score_controller->curl_get_request($score_controller->getUrl());
+  $get_score_validation = json_decode($score_curl, true);
+  
   /** cURL backend exam using query parameter for the name front end will pass query name through json */
   $endpoint = RESULT_URL . '?' . http_build_query(array('exam' => $json['exam'], 'user' => $json['user']));
   
@@ -155,6 +178,7 @@ if (isset($json['exam']) && isset($json['user'])) {
       // set each question and answer to pass to the grader
       $grader->set_question($db_validation['results'][$i]['question']);
       $grader->set_answer($db_validation['results'][$i]['answer']);
+      $grader->set_weight($get_score_validation['questions'][$i]['score']);
 
       // set each question's test cases' input and output to the grader
       for ($j = 0; $j < count($db_validation['results'][$i]['testCases']); $j++) {
@@ -162,12 +186,12 @@ if (isset($json['exam']) && isset($json['user'])) {
         $grader->set_test_output($db_validation['results'][$i]['testCases'][$j]['output']);
 
         // call the test case grader and store the grade
-        $func_score = $grader->grade_test_case($grader->get_test_input(), $grader->get_test_output(), $grader->get_answer(), $grader->get_question());
+        $func_score = $grader->grade_test_case($grader->get_test_input(), $grader->get_test_output(), $grader->get_answer(), $grader->get_question(), $grader->get_weight());
         $final_question_score += $func_score;
       }
       
       // call the function grader and store the grade
-      $test_score = $grader->grade_func_name($grader->get_question(), $grader->get_answer());
+      $test_score = $grader->grade_func_name($grader->get_question(), $grader->get_answer(), $grader->get_weight());
       $final_question_score += $test_score;
 
       // map to the body for put request
---
+++ release-candidate/README.md
@@ -0,0 +1,24 @@
+# Release Candidate TO-DO:
+
+## April 14th 2020:
+* due date
+
+## March 24th 2020:
+* Convert text areas for:
+  * description
+  * student answer area
+  * instructor comments
+* CSS quality of life
+* Persisting
+* Add log out button - kill session, jump to login.html
+* python capture return output in grader.php
+* ability for professor to add a comment per question
+* points per sub item
+* maybe change to drag and drop - I think Nicholson will agree with this
+
+
+###### Example Question: doubleIt()
+* 5 points for name
+* 10 points for test case 1
+* 10 points for test case 2
+* Create realistic test questions/answers/testcases
