333ad5f63287b370113ec800b03a30ac20a526a8
tomgarside <tom31@hotmail.co.uk>
2020-04-27 20:11:10 -0400

    css fixes

--- release-candidate/front-new/Professor.js
+++ release-candidate/front-new/Professor.js
@@ -678,9 +678,12 @@ function addcase() {
     var label = document.createElement('label');
     var inputCase = document.createElement('input');
     var outputCase = document.createElement('input');
-
+    
+    inputCase.setAttribute('class','testcaseInput');
+    outputCase.setAttribute('class','testcaseInput');
     list.setAttribute('id', 'tc' + counter);
     label.setAttribute('for', 'testcase' + counter);
+    label.setAttribute('class', 'testcaseLabel');
     label.innerText = 'Test Case ' + counter + ': ';
     list.appendChild(label);
     list.appendChild(inputCase);
